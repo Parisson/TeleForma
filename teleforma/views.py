41cad973c0ce429251b4e13bb02867bc307831e8
yomguy <yomguy@parisson.com>
2012-06-06 00:40:59 +0200

    update diag

--- teleforma/views.py
+++ teleforma/views.py
@@ -321,7 +321,7 @@ class UsersCourseView(UsersView):
 
     def get_queryset(self):
         self.course = Course.objects.filter(id=self.args[0])
-        return User.objects.filter(student__training__courses__in=self.course)
+        return User.objects.filter(student__written_speciality__in=self.course)
 
     def get_context_data(self, **kwargs):
         context = super(UsersCourseView, self).get_context_data(**kwargs)
