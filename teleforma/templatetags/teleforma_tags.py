f7b401eedcad2404d90c51aaa48309127b0b3426
yomguy <yomguy@parisson.com>
2012-07-19 22:05:37 +0200

    fix host again

--- teleforma/templatetags/teleforma_tags.py
+++ teleforma/templatetags/teleforma_tags.py
@@ -192,7 +192,7 @@ def get_video_id(media):
 def get_host(url, host):
     u = urlparse(url)
     if host == '127.0.0.1' or host == 'localhost':
-        u.netloc = host
-        return u.geturl()
+        nu = u.scheme + '://' + host + path
+        return nu
     else:
         return url
