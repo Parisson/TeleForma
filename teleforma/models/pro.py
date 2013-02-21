8dfae34974388227c1436844b121c5a00a910e24
yomguy <yomguy@parisson.com>
2013-02-21 10:14:45 +0100

    null weight ok

--- teleforma/models/pro.py
+++ teleforma/models/pro.py
@@ -140,7 +140,7 @@ class Question(models.Model):
     description = models.CharField(_('description'), max_length=1024, blank=True)
     question    = tinymce.models.HTMLField(_('question'), blank=True)
     rank        = models.IntegerField(_('rank'), blank=True, null=True)
-    weight      = models.IntegerField(_('weight'), choices=WEIGHT_CHOICES, default=1)
+    weight      = models.IntegerField(_('weight'), choices=WEIGHT_CHOICES, default=1, null=True, blank=True)
     min_nchar   = models.IntegerField(_('minimum numbers of characters'))
     status      = models.IntegerField(_('status'), choices=STATUS_CHOICES, default=3)
 
