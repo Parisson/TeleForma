3e51dab3330cbb38604d1b7440e14a05d19c55c9
Guillaume Pellerin <yomguy@parisson.com>
2013-07-17 16:04:13 +0200

    fix course pull

--- teleforma/views/core.py
+++ teleforma/views/core.py
@@ -245,7 +245,9 @@ class CourseListView(CourseAccessMixin, ListView):
             course = Course.objects.filter(code=course_dict['code'])
             if not course:
                 course = Course()
-                course.from_dict(course_dict)
+            else:
+                course = course[0]
+            course.from_dict(course_dict)
 
 
 class CourseView(CourseAccessMixin, DetailView):
