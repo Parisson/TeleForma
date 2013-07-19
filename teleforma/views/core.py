499efb1ef13353860c1759dfb74463ab61bcd9ce
Guillaume Pellerin <yomguy@parisson.com>
2013-07-19 15:33:25 +0200

    except live message

--- teleforma/views/core.py
+++ teleforma/views/core.py
@@ -543,7 +543,11 @@ class ConferenceRecordView(FormView):
                 except:
                     pass
 
-        live_message(self.conference)
+        
+        try:
+            live_message(self.conference)
+        except:
+            pass
 
         try:
             self.push()
@@ -588,8 +592,10 @@ class ConferenceRecordView(FormView):
                     stream = LiveStream(conference=conference, server=server,
                                         stream_type=stream_type, streaming=True)
                     stream.save()
-
-                live_message(conference)
+                try:
+                    live_message(conference)
+                except:
+                    pass
         else:
             raise 'Error : input must be a conference dictionnary'
 
