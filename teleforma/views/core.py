18b3efc7d0c52617a6502690c029df032e49b903
Guillaume Pellerin <yomguy@parisson.com>
2013-11-19 15:53:36 +0100

    fix warning

--- teleforma/views/core.py
+++ teleforma/views/core.py
@@ -241,7 +241,7 @@ class CourseListView(CourseAccessMixin, ListView):
         return [course.to_dict() for course in Course.objects.filter(department=department)]
 
     def pull(request, organization_name):
-        from teleforma.models import *
+        from teleforma.models import Organization, Department
         organization = Organization.objects.get(name=organization_name)
         departments = Department.objects.filter(organization=organization)
         for department in departments:
