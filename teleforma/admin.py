4e8b4d25eabd44bb06dee35892eb83b8f82afe87
Guillaume Pellerin <yomguy@parisson.com>
2013-12-17 00:00:01 +0100

    fix admin filters

--- teleforma/admin.py
+++ teleforma/admin.py
@@ -71,7 +71,7 @@ class SeminarQuestionInline(admin.StackedInline):
 
 class SeminarAdmin(admin.ModelAdmin):
     inlines = [SeminarQuestionInline,]
-    filter_horizontal = ['professor', 'medias', 'docs_description',
+    filter_vertical = ['professor', 'medias', 'docs_description',
                          'docs_1', 'docs_2', 'docs_correct']
     ordering = ['course', 'rank']
     search_fields = ['course__title', 'title', 'sub_title']
