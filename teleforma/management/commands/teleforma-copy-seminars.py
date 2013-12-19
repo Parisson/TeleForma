e8d82299f40c695826fa162af7a78e6859177534
Guillaume Pellerin <yomguy@parisson.com>
2013-12-19 18:20:20 +0100

    more print

--- teleforma/management/commands/teleforma-copy-seminars.py
+++ teleforma/management/commands/teleforma-copy-seminars.py
@@ -37,11 +37,12 @@ class Command(BaseCommand):
                     print ('dates updated', clone)
 
                     for field in seminar._meta.many_to_many:
+                        print type(field.rel.to)
                         if type(field.rel.to) == Document or type(field.rel.to) == Media:
                             source = getattr(seminar, field.attname)
                             destination = getattr(clone, field.attname)
                             print source, destination
-                            
+
                             for item in source.all():
                                 print item
                                 item.period = from_period
