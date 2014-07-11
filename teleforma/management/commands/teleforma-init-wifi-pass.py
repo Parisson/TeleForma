b299d29a65ed4d8baa72afdfdd7aaff13abec7a0
Guillaume Pellerin <yomguy@parisson.com>
2014-07-11 17:14:30 +0200

    add wifi login and pass to profile

---
+++ teleforma/management/commands/teleforma-init-wifi-pass.py
@@ -0,0 +1,24 @@
+from optparse import make_option
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
+from telemeta.models import *
+from telemeta.util.unaccent import unaccent
+from teleforma.exam.models import *
+import logging
+import codecs
+import random, string
+
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+	return ''.join(random.choice(chars) for _ in range(size))
+
+class Command(BaseCommand):
+    help = "init all user wifi pass"
+
+    def handle(self, *args, **options):
+    	for user in User.objects.all():
+    		profile = user.profile.get()
+    		profile.wifi_login = user.username + '.' + args[-1]
+    		profile.wifi_pass = id_generator(8)
+    		profile.save()
