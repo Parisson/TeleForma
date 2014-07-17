f52daa6dcdf1b759b08967329fa29a34555bbb39
Guillaume Pellerin <yomguy@parisson.com>
2014-07-17 13:40:17 +0200

    add messages

--- teleforma/management/commands/teleforma-exam-submit-scripts.py
+++ teleforma/management/commands/teleforma-exam-submit-scripts.py
@@ -1,3 +1,4 @@
+
 from optparse import make_option
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
@@ -8,12 +9,28 @@ from telemeta.util.unaccent import unaccent
 from teleforma.exam.models import *
 import logging
 import codecs
+import time
+
+
+class Logger:
+    """A logging object"""
+
+    def __init__(self, file):
+        self.logger = logging.getLogger('teleforma')
+        self.hdlr = logging.FileHandler(file)
+        self.formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+        self.hdlr.setFormatter(self.formatter)
+        self.logger.addHandler(self.hdlr)
+        self.logger.setLevel(logging.INFO)
 
 
 class Command(BaseCommand):
     help = "submit all script to Box View"
 
     def handle(self, *args, **options):
+    	logger = Logger(args[0])
     	for script in Script.objects.filter(status=2):
-    		print unicode(script)
+    		logger.logger.info(script.title)
     		script.submit()
+    		logger.logger.info('OK')
+    		time.sleep(10)
