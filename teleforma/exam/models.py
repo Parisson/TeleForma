fd54f3538151ef15d5015679be9ae52bbb211f86
Guillaume Pellerin <yomguy@parisson.com>
2014-07-19 17:29:18 +0200

    invert script ordering

--- teleforma/exam/models.py
+++ teleforma/exam/models.py
@@ -215,7 +215,7 @@ class Script(BaseResource):
     class Meta(MetaCore):
         verbose_name = _('Script')
         verbose_name_plural = _('Scripts')
-        ordering = ['-date_added']
+        ordering = ['date_added']
 
     @property
     def box_admin_url(self):
