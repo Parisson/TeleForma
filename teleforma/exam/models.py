83d5a851e219d973f6ca84815f2bf773406d590b
Guillaume Pellerin <yomguy@parisson.com>
2015-07-14 12:50:57 +0200

    add script "backup" state

--- teleforma/exam/models.py
+++ teleforma/exam/models.py
@@ -60,7 +60,7 @@ import crocodoc
 crocodoc.api_token = settings.BOX_API_TOKEN
 
 SCRIPT_STATUS = ((0, _('rejected')), (1, _('draft')), (2, _('submitted')),
-                (3, _('pending')),(4, _('marked')), (5, _('read')) )
+                (3, _('pending')),(4, _('marked')), (5, _('read')), (6, _('backup')) )
 
 REJECT_REASON = (('unreadable', _('unreadable')),
                 ('bad orientation', _('bad orientation')),
