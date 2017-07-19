3e9b78bfa73370fcfe8128cc772a281974f91101
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2017-07-19 09:52:16 +0200

    goto 1.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
   long_description = open('README.rst').read(),
   author = "Guillaume Pellerin",
   author_email = "yomguy@parisson.com",
-  version = '1.0',
+  version = '1.1',
   install_requires = [
         'django>=1.4',
         'telemeta',
