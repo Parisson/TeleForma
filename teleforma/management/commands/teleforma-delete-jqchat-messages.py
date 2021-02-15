fafd9e842e9002ed67c7aaed8c8af99187e5b35d
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2021-02-15 11:09:40 +0100

    add django command to delete messages older than 1 year

---
+++ teleforma/management/commands/teleforma-delete-jqchat-messages.py
@@ -0,0 +1,20 @@
+
+from optparse import make_option
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth.models import User
+from jqchat.models import Message
+import datetime
+
+
+class Command(BaseCommand):
+    help = "Delete jqchat messages older than 1 year"
+
+    def handle(self, *args, **options):
+        date_now = datetime.datetime.now()
+        date_old = date_now.replace(year=date_now.year-1)
+        print(date_old)
+        messages = Message.objects.filter(created__lte=date)
+        print(messages.count())
+        for message in messages:
+            message.delete()
