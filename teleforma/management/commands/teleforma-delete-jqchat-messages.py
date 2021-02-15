d32e4efc4c8ff6fe6a253414e4da41c0667586a2
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2021-02-15 11:11:05 +0100

    bugfix

--- teleforma/management/commands/teleforma-delete-jqchat-messages.py
+++ teleforma/management/commands/teleforma-delete-jqchat-messages.py
@@ -14,7 +14,7 @@ class Command(BaseCommand):
         date_now = datetime.datetime.now()
         date_old = date_now.replace(year=date_now.year-1)
         print(date_old)
-        messages = Message.objects.filter(created__lte=date)
+        messages = Message.objects.filter(created__lte=date_old)
         print(messages.count())
         for message in messages:
             message.delete()
