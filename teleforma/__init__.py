aa2a5036f6e73d1efc49d0df810a3897a45d1507
Guillaume Pellerin <guillaume.pellerin@parisson.com>
2021-06-22 08:36:30 +0200

    bump to 2.0

--- teleforma/__init__.py
+++ teleforma/__init__.py
@@ -9,12 +9,12 @@ U{http://parisson.com/products/teleforma}
 """
 
 __docformat__ = 'epytext en'
-__version__ = '1.4'
+__version__ = '2.0'
 __url__ = 'http://parisson.com/products/teleforma'
 __copyright__ = '(C) 2011-2017 Parisson'
 __license__ = 'CeCILL-v2'
 __license_long__ = """
-Copyright (C) Parisson SARL (2011-2017)
+Copyright (C) Parisson SARL (2011-2021)
 
 <yomguy@parisson.com>
 
