dd498d5154f68818d68c9c642934ab839dea6e91
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2021-06-22 09:12:05 +0200

    add command to delete old scripts and annotations

---
+++ teleforma/management/commands/teleforma-delete-annotations.py
@@ -0,0 +1,31 @@
+
+from optparse import make_option
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth.models import User
+from jqchat.models import Message
+import datetime
+from teleforma.exam.models import Script
+from pdfannotator.models import Annotation, AnnotationComment
+
+
+class Command(BaseCommand):
+    help = "Delete script annotations from previous year"
+
+    def handle(self, *args, **options):
+        date = datetime.datetime.now()
+        date_old = date_now.replace(year=date_now.year-1, month=12, day=31)
+        print(date_old)
+        scripts = Script.objects.filter(date_submitted__lte=date_old)
+        print(scripts.count())
+
+        for script in scripts:
+            annotations = Annotation.objects.filter(uuid=script.uuid)
+            for annotation in annotations:
+                annotation.delete()
+            annotation_comments = AnnotationComment.objects.filter(uuid=script.uuid)
+            for annotation_comment in annotation_comments:
+                annotation_comment.delete()
+            script.delete()
+
+        
