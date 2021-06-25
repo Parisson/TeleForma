97dd07a54e6b6c9a43f47722b9a4a23e102a2704
Gael Le Mignot <gael@pilotsystems.net>
2021-06-25 09:33:11 +0200

    Fixed password change template / urls

--- teleforma/urls.py
+++ teleforma/urls.py
@@ -113,7 +113,7 @@ urlpatterns = [
     url(r'^accounts/password_change/$', PasswordChangeView.as_view(template_name='registration/password_change_form.html'),
         name="teleforma-password-change"),
     url(r'^accounts/password_change_done/$', PasswordChangeDoneView.as_view(
-        template_name='registration/password_change_done.html'), name="teleforma-password-change-done"),
+        template_name='registration/password_change_done.html'), name="password_change_done"),
 
     url(r'^accounts/password_reset/$', PasswordResetView.as_view(template_name='registration/password_reset_form.html',
                                                                  email_template_name='registration/password_reset_email.html'), name="teleforma-password-reset"),
