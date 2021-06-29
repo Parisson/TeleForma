021e921ca5d73fc8f7f14c8debb22fc016064486
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2021-06-29 15:25:26 +0200

    bugfix

--- teleforma/urls.py
+++ teleforma/urls.py
@@ -196,7 +196,7 @@ urlpatterns = [
         ConferenceView.as_view(
             template_name="teleforma/course_conference_audio.html")
         ),
-        name="teleforma-conference-audio"),
+        name="teleforma-conference-audio",
     url(r'^desk/periods/(?P<period_id>.*)/conferences/list/$', ConferenceListView.as_view(),
         name="teleforma-conferences"),
 
