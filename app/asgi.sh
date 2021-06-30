432866fa23ca8ad7a8938d25ee5d9a3e0511bf9c
Gael Le Mignot <gael@pilotsystems.net>
2021-06-30 15:57:44 +0200

    Splitting dev into asgi/wsgi too so it's closer to prod

--- app/asgi.sh
+++ app/asgi.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
 
-daphne -b 0.0.0.0 -p 8000 asgi:application
+# paths
+app='/srv/app'
+manage=$app'/manage.py'
+static='/srv/static/'
+media='/srv/media/'
+src='/srv/src/'
+
+if [ "$1" = "--runserver" ]; then
+    python $manage runserver 0.0.0.0:8000
+else
+    # static files auto update
+    # watchmedo shell-command --patterns="$patterns" --recursive \
+    #     --command='python '$manage' collectstatic --noinput' $app &
+    daphne -b 0.0.0.0 -p 8000 asgi:application
+fi
+
 
