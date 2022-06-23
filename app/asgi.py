fffaf30dc283e4f3b2d3e38bb42429407d8e9611
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2022-06-23 17:25:23 +0200

    add notification routing into asgi from routing

--- app/asgi.py
+++ app/asgi.py
@@ -12,6 +12,7 @@ from teleforma.ws import chat
 
 websocket_urlpatterns = [
     re_path(r'ws/chat/(?P<room_name>\w+)/$', chat.ChatConsumer.as_asgi()),
+    re_path(r'ws/notification/(?P<user_id>\w+)/$', notification.NotificationConsumer.as_asgi()),
 ]
 
 
@@ -23,8 +24,3 @@ application = ProtocolTypeRouter({
         )
     ),
 })
-
-
-
-
-
