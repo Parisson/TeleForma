cef446b96a32bd750beb27b6e94f107899a3c978
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2023-01-16 16:26:59 +0100

    add Site update command

---
+++ teleforma/management/commands/teleforma-update-site.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand
+from django.contrib.sites.models import Site
+from django.conf import settings
+
+
+class Command(BaseCommand):
+    help = "Change Site domain to another"
+    
+    def handle(self, *args, **options):
+        to_domain = args[-1]
+        from_domain = args[-2]
+
+        site = Site.objects.get(domain=from_domain)
+        site.domain = to_domain
+        site.save()
