9b73b38ac07ba9a2d979020986d474857bfb6562
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2023-03-17 09:56:26 +0100

    fix save method

--- teleforma/forms.py
+++ teleforma/forms.py
@@ -222,7 +222,7 @@ class UserForm(ModelForm):
 class UserUseYourLawOriginForm(UserForm):
 
     def save(self, commit=True):
-        super(UserUseYourLawOriginForm, self).__save__(*args, **kwargs)
+        super(UserUseYourLawOriginForm, self).save(*args, **kwargs)
         origin = Origin.objects.get_or_create(name="UseYourLaw")
         self.profile.origin = origin
 
