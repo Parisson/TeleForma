a285e376dafa79e84a036b8b5f66b79c256e3c18
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2023-03-17 10:11:52 +0100

    fix filter

--- teleforma/admin.py
+++ teleforma/admin.py
@@ -238,7 +238,7 @@ class UserProfileAdmin(UserAdmin):
     list_display = UserAdmin.list_display + (
         'user_actions', 
     )
-    list_filter = ['origin',]
+    list_filter = ['profile__origin',]
 
     def user_actions(self, obj):
         return format_html(
